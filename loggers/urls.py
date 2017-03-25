6b733c89a7e94288d7e090431ebefcd411622a31
njerucyrus <njerucyrus5@hotmail.com>
2017-03-25 20:58:33 +0300

    added url for api

--- loggers/urls.py
+++ loggers/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls import include,url
+from django.conf.urls import include, url
 from django.contrib import admin
 from inventory.admin import admin_site
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -7,9 +7,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^abcdefgh/', include(admin.site.urls)),
     url(r'^inventory/', admin_site.urls),
-    #url(r'^', include('inventory.urls')),
-
+    # url(r'^', include('inventory.urls')),
+    url(r'^api/', include('api.v1.urls', namespace='apiv1')),
 
 
 ]
-urlpatterns=format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatterns)
